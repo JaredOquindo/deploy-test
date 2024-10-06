833b6967744c30a8f70bd4834b010be4ca03669e
JaredOquindo <158650266+JaredOquindo@users.noreply.github.com>
2024-10-07 03:06:16 +0800

    Update Login.jsx

--- LOGIN_REGISTER/client/src/Login.jsx
+++ LOGIN_REGISTER/client/src/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3000/login', {email, password})
+        axios.post('gymbro-sigma.vercel.app/login', {email, password})
         .then(result => {
             console.log(result)
             if(result.data === "Success"){
